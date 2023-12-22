9605d82d9a9569d32369b84c99ae3d10880e4237
reinchisdog <reindf164@gmail.com>
2023-12-23 03:39:06 +0800

    Day 12-14 - Assignment

--- frontend-lessons/javascript-tasks/day-13/index.js
+++ frontend-lessons/javascript-tasks/day-13/index.js
@@ -5,6 +5,10 @@
 // 1. Declare a variable named `currentLesson` and assign it the value 'JavaScript Fundamentals'.
 // 2. Print the value of `currentLesson` to the console.
 
+const currentLesson = 'JavaScript Fundamentals';
+
+console.log(currentLesson);
+
 // Task 2: Data Types Exploration
 
 // 3. Create a variable named `favoriteNumber` and assign it a number of your choice.
@@ -13,9 +17,36 @@
 // 6. Create a variable named `nothing` and assign it a value of `null`.
 // 7. Create a variable named `undefinedVar` without assigning any value to it.
 
+const favoriteNumber = 16;
+const favoriteWord = 'Banana';
+let isJavaScriptFun = true;
+const nothing = null;
+let undefinedVar;
+
+console.log(favoriteNumber,favoriteWord,isJavaScriptFun,nothing,undefinedVar);
+
 // Task 3: Logical Operators in Action
 
 // 8. Create two variables, `isMorning` and `isRaining`, and assign them boolean values.
 // 9. Use the logical **AND** (`&&`) operator to check if it's morning and raining. Print appropriate messages based on the result.
 // 10. Use the logical **OR** (`||`) operator to check if it's either morning or raining. Print appropriate messages based on the result.
 // 11. Use the logical **NOT** (`!`) operator to invert the value of `isMorning`. Print the new value.
+
+let isMorning = false;
+let isRaining = true;
+
+if(isMorning && isRaining){
+     console.log("It is both morning and raining.");
+}
+else {
+     console.log("It is only either morning or raining, or not both.");
+}
+
+if(isMorning || isRaining){
+     console.log("It is either morning or raining.");
+}
+else {
+     console.log("It is neither morning nor raining.");
+}
+
+console.log(!isMorning);
--- frontend-lessons/javascript-tasks/day-14/index.js
+++ frontend-lessons/javascript-tasks/day-14/index.js
@@ -6,3 +6,88 @@
 // 4. Create a variable named `time` and assign it a value representing the current hour (in 24-hour format).
 // 5. Use `else if` statements to greet the user based on the time of day (morning, afternoon, evening).
 // 6. Create a switch statement for the variable `day`. Print a message based on the day of the week.
+
+let temperature = 36.9;
+
+if (temperature > 30){
+     console.log("Temperature is greater than 30");
+}
+else {
+     console.log("Temperature is equal or less than 30");
+}
+
+let time = 1345;
+
+if ((time >= 0 && time <= 59) ||
+     (time >= 100 && time <= 159) ||
+     (time >= 200 && time <= 259) ||
+     (time >= 300 && time <= 359) ||
+     (time >= 400 && time <= 459) ||
+     (time >= 500 && time <= 559) ||
+     (time >= 600 && time <= 659) ||
+     (time >= 700 && time <= 759) ||
+     (time >= 800 && time <= 859) ||
+     (time >= 900 && time <= 959) ||
+     (time >= 1000 && time <= 1059) ||
+     (time >= 1100 && time <= 1159)){
+     console.log("Morning");
+}
+
+else if ((time >= 1200 && time <= 1259) ||
+     (time >= 1300 && time <= 1359) ||
+     (time >= 1400 && time <= 1459) ||
+     (time >= 1500 && time <= 1559) ||
+     (time >= 1600 && time <= 1659) ||
+     (time >= 1700 && time <= 1759)){
+     console.log("Afternoon");
+}
+
+else if ((time >= 1800 && time <= 1859) ||
+     (time >= 1900 && time <= 1959) ||
+     (time >= 2000 && time <= 2059) ||
+     (time >= 2100 && time <= 2159) ||
+     (time >= 2200 && time <= 2259) ||
+     (time >= 2300 && time <= 2359)){
+     console.log("Evening");
+}
+
+else {
+     console.log("Not a Valid Time");
+}
+
+let day = 'Saturday';
+
+switch (day){
+     case 'Monday':{
+          console.log("Monday");
+          break;
+     }  
+     case 'Tuesday':{
+          console.log("Tuesday");
+          break;
+     }
+     case 'Wednesday':{
+          console.log("Wednesday");
+          break;
+     }
+     case 'Thursday':{
+          console.log("Thursday");
+          break;
+     }     
+     case 'Friday':{
+          console.log("Friday");
+          break;
+     }     
+     case 'Saturday':{
+          console.log("Saturday");
+          break;
+     }     
+     case 'Sunday':{
+          console.log("Sunday");
+          break;
+     }    
+     default:{
+          console.log("Not a Valid Day");
+          break;
+     }
+}
