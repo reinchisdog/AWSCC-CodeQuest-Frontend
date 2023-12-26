880a74de5007be11dd2d9f88d178693146e01900
reinchisdog <reindf164@gmail.com>
2023-12-27 04:32:21 +0800

    Added gamemode options (CPU & PVP mode)

--- projects/03-rock-paper-scissors/index.js
+++ projects/03-rock-paper-scissors/index.js
@@ -1,6 +1,8 @@
 //Variables
 const choices = ["Rock", "Paper", "Scissors"];
 
+const choiceButtons = document.getElementsByClassName('choice-btn');
+
 const player1ChoiceDisplay = document.getElementById('player1-choice-display');
 const player1ScoreDisplay = document.getElementById('player1-score');
 const player1Buttons = document.getElementById('player1-choices');
@@ -10,7 +12,13 @@ const player2ScoreDisplay = document.getElementById('player2-score');
 const player2Buttons = document.getElementById('player2-choices');
 
 const gameStatusDisplay = document.getElementById('game-status');
+
+const bottomContainer = document.getElementById('bottom-container');
 const resetGameButton = document.getElementById('reset-game-btn');
+const gameModeButton = document.getElementById('gamemode-option')
+const nextRoundButton = document.getElementById('next-round-btn');
+const versusCPU = document.getElementById('v-CPU');
+const versusPlayer = document.getElementById('v-PLY');
 
 const rockBtn = document.getElementById('rock-btn');
 const paperBtn = document.getElementById('paper-btn');
@@ -18,6 +26,8 @@ const scissorsBtn = document.getElementById('scissors-btn');
 
 let choice1, choice2;
 let p1Score = 0, p2Score = 0;
+
+let isPlayerMode = null;
 
 //Event Listeners for Buttons
 rockBtn.addEventListener("click", function () {
@@ -32,7 +42,43 @@ scissorsBtn.addEventListener("click", function () {
      choiceProcess(2);
 });
 
+versusCPU.addEventListener("click", function (){
+     isPlayerMode = false;
+     resetGameButton.style.display = 'block';
+     gameModeButton.style.display = 'none';
 
+     for (let i=0; i<3; i++){
+          choiceButtons[i].style.display = 'block';
+     }
+});
+
+versusPlayer.addEventListener("click", function (){
+     isPlayerMode = true;
+     resetGameButton.style.display = 'block';
+     gameModeButton.style.display = 'none';
+
+     for (let i=0; i<3; i++){
+          choiceButtons[i].style.display = 'block';
+     }
+});
+
+nextRoundButton.addEventListener("click", function (){
+     while(player2Buttons.childNodes.length > 0){
+          player1Buttons.appendChild(player2Buttons.childNodes[0]);
+     }
+
+     for (let i=0; i<3; i++){
+          choiceButtons[i].style.display = 'block';
+     }
+
+     nextRoundButton.style.display = 'none';
+     bottomContainer.style.top = '86%';
+     player1ChoiceDisplay.innerHTML = '';
+     player2ChoiceDisplay.innerHTML = '';
+
+     gameStatusDisplay.style.width = '700px';
+     gameStatusDisplay.textContent = 'SHOW YOUR HAND TO START THE GAME!';
+});
 
 resetGameButton.addEventListener("click", function () { 
      p1Score = 0, p2Score = 0;
@@ -42,14 +88,25 @@ resetGameButton.addEventListener("click", function () {
 
      player1ChoiceDisplay.innerHTML = '';
      player2ChoiceDisplay.innerHTML = '';
+     nextRoundButton.style.display = 'none';
+     bottomContainer.style.top = '86%';
+     resetGameButton.style.display = 'none';
+     gameModeButton.style.display = 'block';
 
      while(player2Buttons.childNodes.length > 0){
           player1Buttons.appendChild(player2Buttons.childNodes[0]);
      }
 
+     for (let i=0; i<3; i++){
+          choiceButtons[i].style.display = 'none';
+     }
+
      gameStatusDisplay.style.width = '700px';
-     gameStatusDisplay.textContent = "SHOW YOUR HAND TO START THE GAME!";
+     gameStatusDisplay.textContent = 'SHOW YOUR HAND TO START THE GAME!';
 });
+
+
+
 
 //Function: for comparing choices to draw conclusion
 function compareProcess(){
@@ -74,43 +131,66 @@ function compareProcess(){
      }
 }
 
+
 //Function: for setting the process after choosing from Player 1 Buttons
 function p1Turn(){
      while(player1Buttons.childNodes.length > 0){
           player2Buttons.appendChild(player1Buttons.childNodes[0]);
      }
 
-     gameStatusDisplay.style.width = "500px"
-     gameStatusDisplay.textContent = "Waiting for PLAYER 2's TURN...";
+     if (isPlayerMode == false){
+          for (let i=0; i<3; i++){
+               choiceButtons[i].style.display = 'none';
+          } 
+          const randomChoice = Math.floor(Math.random() * choices.length);
+
+          player2ChoiceDisplay.textContent = choices[randomChoice];
+
+          choice2 = randomChoice;
+
+          p2Turn();
+     }
+     else {
+          gameStatusDisplay.style.width = "500px"
+          gameStatusDisplay.textContent = "Waiting for PLAYER 2's TURN...";
+     }
 }
 
 //Function: for setting the process after choosing from Player 2 Buttons
 function p2Turn(){
-     while(player2Buttons.childNodes.length > 0){
-          player1Buttons.appendChild(player2Buttons.childNodes[0]);
-     }
-
      compareProcess();
 
-     player1ChoiceDisplay.innerHTML = '';
-     player2ChoiceDisplay.innerHTML = '';
+     for (let i=0; i<3; i++){
+          choiceButtons[i].style.display = 'none';
+     }
+
+     nextRoundButton.style.display = 'block';
+     bottomContainer.style.top = '78.4%';
+}
+
+//Function: for setting the process of Player 2 (CPU)
+function CPUTurn(){
+     compareProcess();
+
+     for (let i=0; i<3; i++){
+          choiceButtons[i].style.display = 'none';
+     }
+
+     nextRoundButton.style.display = 'block';
+     bottomContainer.style.top = '78.4%';
 }
 
 //Function: that sets the Process of the game
 function choiceProcess(choice){
-     if (player1ChoiceDisplay.childElementCount == 0){
-          const player1Choice = document.createElement('h1');
-          player1Choice.textContent = choices[choice];
-          player1ChoiceDisplay.appendChild(player1Choice);
+     if (!player1ChoiceDisplay.textContent.trim()){
+          player1ChoiceDisplay.textContent = choices[choice];
 
           choice1 = choice;
 
           p1Turn();
      }
-     else{   
-          const player2Choice = document.createElement('h1');
-          player2Choice.textContent = choices[choice];
-          player2ChoiceDisplay.appendChild(player2Choice);
+     else if (isPlayerMode == true){   
+          player2ChoiceDisplay.textContent = choices[choice];
 
           choice2 = choice;
 
